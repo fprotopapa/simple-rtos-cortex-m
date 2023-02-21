43ffca68ea504fd7dd9ade514b4493fd01cd2a53
Fabbio Protopapa <fprotopapa.git@gmail.com>
2023-02-21 15:38:11 +0100

    Simple Manual Context Switch

--- src/main.c
+++ src/main.c
@@ -3,19 +3,45 @@
 #include "timer.h"
 #include <stdint.h>
 
+void task1();
+void task2();
+int task1_main();
+int task2_main();
+
 int main (void) {
     led_init();
     uart_init();
     timer_init();
-    char buff[] = "Hello World.\n\r";
-    uart_print(buff, sizeof(buff));
+    volatile uint32_t cnt = 1;
     while (1) {
-        timer_delay_sec(1);
-        led_on();
-        uart_print(buff, sizeof(buff));
-        timer_delay_sec(1);
-        led_off();
+        if(cnt) {
+            task1_main();
+        } else {
+            task2_main();   
+        }  
     }
     return 0;
 }
+void task1() {
+    char buff[] = "Task 1.\n\r";
+    uart_print(buff, sizeof(buff));
+}
 
+void task2() {
+    char buff[] = "Task 2.\n\r";
+    uart_print(buff, sizeof(buff));
+}
+
+int task1_main() {
+    while(1) {
+        timer_delay_sec(1);
+        task1();
+    }
+}
+
+int task2_main() {
+    while(1) {
+        timer_delay_sec(1);
+        task2();
+    }
+}
